e8ed1fd6596095293cab315e74c1c4dda9f6395b
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-14 17:33:27 +0300

    дод createSlice === reducer, actions, actions.type

--- src/redux/reducers/reducer.js
+++ src/redux/reducers/reducer.js
@@ -1,4 +1,4 @@
-import { ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACTS } from '../actions/contacts-types';
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   contacts: {
@@ -7,31 +7,54 @@ const initialState = {
   },
 };
 
-export const contactsReducer = (state = initialState, actions) => { // actions { id, name, number }
-  switch (actions.type) {
-    case ADD_CONTACT: // перевіряєм type який в actions.type -- type: ADD_CONTACT,'ADD_CONTACT'
-      return {
-        contacts: {
-          ...state.contacts, // розпиляєм попередні дані з обєкта contacts щоб не мутувати state
-          items: [actions.payload, ...state.contacts.items],// так новий контакт буде дод на верх списку
-        },
-      };
-    case FILTER_CONTACTS: // перевіряєм type який в actions.type -- type: FILTER_CONTACTS,'FILTER_CONTACTS'
-      return {
-        contacts: {
-          ...state.contacts, // розпиляєм попередні дані з обєкта contacts щоб не мутувати state
-          filter: actions.payload, // літери за якими шукаєм контакти в ContactsList
-        },
-      };
-    case DELETE_CONTACT: // перевіряєм type який в actions.type -- type: DELETE_CONTACT,'DELETE_CONTACT'
-      return {
-        contacts: {
-          ...state.contacts, // розпиляєм попередні дані з обєкта contacts щоб не мутувати state
-          items: [...actions.payload],// розпиляєм новий відфільтрований масив БЕЗ контакта який видалили
-        },
-      };
-    default:
-      return state;
-  }
-};
+const contactSlice = createSlice({
+    name: 'contacts',
+    initialState,
+    reducers: {
+      addContact: (state, actions) => state.contacts.items.push(actions.payload),
+      filterContacts: ({ contacts }, actions) => contacts.filter = actions.payload,
+      deleteContact: ({ contacts }, actions) => [...contacts.items, ...actions.payload],
+    },
+  })
+;
 
+export const { addContact, filterContacts, deleteContact } = contactSlice.actions;
+export default contactSlice.reducer;
+
+// import { ADD_CONTACT, DELETE_CONTACT, FILTER_CONTACTS } from '../actions/contacts-types';
+//
+// const initialState = {
+//   contacts: {
+//     items: [],
+//     filter: '',
+//   },
+// };
+//
+// export const contactsReducer = (state = initialState, actions) => { // actions { id, name, number }
+//   switch (actions.type) {
+//     case ADD_CONTACT: // перевіряєм type який в actions.type -- type: ADD_CONTACT,'ADD_CONTACT'
+//       return {
+//         contacts: {
+//           ...state.contacts, // розпиляєм попередні дані з обєкта contacts щоб не мутувати state
+//           items: [actions.payload, ...state.contacts.items],// так новий контакт буде дод на верх списку
+//         },
+//       };
+//     case FILTER_CONTACTS: // перевіряєм type який в actions.type -- type: FILTER_CONTACTS,'FILTER_CONTACTS'
+//       return {
+//         contacts: {
+//           ...state.contacts, // розпиляєм попередні дані з обєкта contacts щоб не мутувати state
+//           filter: actions.payload, // літери за якими шукаєм контакти в ContactsList
+//         },
+//       };
+//     case DELETE_CONTACT: // перевіряєм type який в actions.type -- type: DELETE_CONTACT,'DELETE_CONTACT'
+//       return {
+//         contacts: {
+//           ...state.contacts, // розпиляєм попередні дані з обєкта contacts щоб не мутувати state
+//           items: [...actions.payload],// розпиляєм новий відфільтрований масив БЕЗ контакта який видалили
+//         },
+//       };
+//     default:
+//       return state;
+//   }
+// };
+//

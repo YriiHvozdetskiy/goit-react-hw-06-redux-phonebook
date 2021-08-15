87e8340eedbb3ac559fe5d477eb3734e1e696873
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-08-15 10:40:03 +0300

    зробили щоб не весь state показувався в localStorage а тільки наші items(контакти), filter виключили до допомогою blackList

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,13 +9,9 @@ import {
   REGISTER,
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './reducers/reducer';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
 // це щоб ігнурувало ці actions (костильок) [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
 const middleware = (getDefaultMiddleware) =>
   getDefaultMiddleware({
@@ -24,16 +20,16 @@ const middleware = (getDefaultMiddleware) =>
     },
   });
 
-// головний reducer де буде зберігатися reduceru для всіх задач (всі reduceru приложенія)
-// combineReducers потрібний щоб працював persistReducer
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  blacklist:['filter'], // виключаєм свойство filter з нашого reducera з localStorage (не буде показуватися)
+};
 
 const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+  },
   middleware,
   devTools: process.env.NODE_ENV === 'development', // потрібно щоб тулзи працювали тільки в розробці
 });
